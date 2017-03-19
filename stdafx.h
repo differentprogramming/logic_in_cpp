f0ccf8d9a5108ff358c5cec189357205534b23ce
Joshua Scholar <joshuascholar@gmail.com>
2017-03-19 12:46:00 -0700

    Add files via upload

---
+++ stdafx.h
@@ -0,0 +1,32 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+
+#include <stdexcept>
+#include <utility>
+#include <boost/any.hpp>
+#include <vector>
+#include <boost/regex.hpp>
+#include <boost/variant.hpp>
+#include <boost/variant/recursive_variant.hpp>
+#include <boost/intrusive_ptr.hpp>
+#include <boost/smart_ptr/intrusive_ref_counter.hpp>
+#include <boost/flyweight.hpp>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+#include <functional>
+#include <map>
+#include <stdint.h>
+//#include "libtcc.h"
